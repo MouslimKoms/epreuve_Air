44f1d564f76c41aa37c375e82cf2893056be8341
MouslimKoms <140436051+MouslimKoms@users.noreply.github.com>
2024-02-12 21:50:50 +0100

    Add files via upload

--- split_en_fonction.py
+++ split_en_fonction.py
@@ -1,20 +1,18 @@
 import sys 
 
 def ma_fonction(string_a_couper, string_separateur):
-    if not isinstance(string_a_couper, str) or not isinstance(string_separateur, str):
-        print("Error: Les arguments doivent etre des chaines de caractères.")
+    if len(sys.argv) != 3:
+      print("Error: Le programme nécessite deux arguments.")
+      sys.exit(1)
+    tableau = string_a_couper.split(string_separateur)
+    return tableau
+
+if __name__=="__main__":
+    if len(sys.argv) != 3:
+        print("Error: Le programme nécessite deux arguments .")
         sys.exit(1)
-        
-    tableau = [ ] 
-    element = " "
-    for char in string_a_couper:
-        if char != string_separateur:
-            element += char
-        else:
-            tableau.append(element)
-            element = " "
-    tableau.append(element)
-    return tableau 
-    
+    resultat = ma_fonction(sys.argv[1], sys.argv[2])
+    print(resultat)
+
             
 
