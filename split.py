3dc0401e8b21912e1aa1b32ebe5283c63bb6c04f
MouslimKoms <140436051+MouslimKoms@users.noreply.github.com>
2024-02-12 22:14:27 +0100

    Add files via upload

--- split.py
+++ split.py
@@ -1,27 +1,19 @@
 import sys
 
-
 def ma_fonction(string_a_couper , string_separateur):
-    if not string_a_couper or not string_separateur:
-        print("Erreur : Les arguments ne doivent pas etre vides")
+    
+    if len(sys.argv) != 2:
+        print ("Error: Le programme nécessiste une chaine de caractère à découper en argument.")
         sys.exit(1)
-    tableau = []
-    mot = ""
-    for char in string_a_couper:
-        if char in string_separateur:
-            if mot:
-                tableau.append(mot)
-                mot = ""
-        else:
-            mot += char
-    if mot:
-        tableau.append(mot)
+    
+    tableau = string_a_couper.split()
     return tableau
 
-if len(sys.argv) != 3:
-    print("Erreur : Veuillez fournir une chaine a découper et un séparateur")
-    sys.exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Error : Le programme nécessiste une chaine de caractère à découper en argument.")
+        sys.exit(1)
+    resultat = ma_fonction(sys.argv[1], " ")
+    print(resultat)
     
     
-resultat = ma_fonction(sys.argv[1], sys.argv[2])
-print("Résultat du découpage :",resultat)
